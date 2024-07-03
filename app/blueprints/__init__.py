b26036cae549a6998f5e3ebc872c695e87b62964
BlackBorada <BlackBorada1@yandex.ru>
2024-07-03 13:23:14 +0500

    Added import profile and users

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -2,15 +2,17 @@ from flask import Blueprint, render_template
 
 from .auth import auth_bp
 from .task import task_bp
+from .profile import profile_bp
+from .users import users_bp
 
 
 def create_blueprint():
     blueprint = Blueprint("index", __name__)
 
-
     @blueprint.route("/")
     def index():
         return render_template("index.html")
+
     return blueprint
 
 
