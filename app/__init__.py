0d13bed6c3d0ee921bf7e5033d59616cbb5f51b6
BlackBorada <BlackBorada1@yandex.ru>
2024-07-03 13:27:51 +0500

    Added imports profile and users.

--- app/__init__.py
+++ app/__init__.py
@@ -7,25 +7,25 @@ from flask_login import LoginManager
 from config import config
 
 
-
 db = SQLAlchemy()
 bootstrap = Bootstrap()
 migrate = Migrate()
 login = LoginManager()
 
-login.login_view = 'auth.login' # type: ignore
+login.login_view = "auth.login"  # type: ignore
+
 
 @login.user_loader
 def load_user(user_id):
-    from app.models import User
-    return User.query.get(int(user_id))
+    from app.models import Users
+
+    return Users.query.get(int(user_id))
 
 
 def create_app(config_type=None):
     if config_type is None:
         config_type = "development"
     app = Flask(__name__)
-
 
     app.config.from_object(config[config_type])
 
@@ -34,15 +34,18 @@ def create_app(config_type=None):
     migrate.init_app(app, db)
     login.init_app(app)
 
-    from .blueprints import main_bp, auth_bp, task_bp
+    from .blueprints import main_bp, auth_bp, task_bp, profile_bp, users_bp
+
     app.register_blueprint(main_bp)
+    app.register_blueprint(users_bp, url_prefix="/users")
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(task_bp, url_prefix="/task")
+    app.register_blueprint(profile_bp, url_prefix="/profile")
 
     with app.app_context():
         db.create_all()
 
-
     return app
+
 
 app = create_app()
